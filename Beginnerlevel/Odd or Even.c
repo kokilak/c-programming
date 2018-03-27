9605840d284228a3401f6c2e314e30c3487bf15e
kokilak <37831135+kokilak@users.noreply.github.com>
2018-03-27 14:49:40 +0530

    Create Odd or Even.c

---
+++ Beginnerlevel/Odd or Even.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main(void) {
+	int n;
+	printf("\nenter a number");
+	scanf("%d",&n);
+	if(n%2==0){
+		printf("\nThe given number is even");
+	}
+	else{
+		printf("\nThe given number is odd");
+	}
+ 
+ 
+	return 0;
+}
+ 
